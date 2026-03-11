805343a0b9f6dc830c5485cbea9845bf09c18f6b
thiagofalima <thiagofalima@hotmail.com>
2026-03-11 17:58:26 -0300

    fix(container): fix Container height property line

--- app/src/components/Container.tsx
+++ app/src/components/Container.tsx
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 export const Container = styled.main`
     width: 100vw;
-    height 100vh;
+    height: 100vh;
     display: flex;
     flex-direction: column;
     justify-content: center;

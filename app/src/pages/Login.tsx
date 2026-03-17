785bd91d90ec950b0254b405908cd84f0e5a2931
thiagofalima <thiagofalima@hotmail.com>
2026-03-17 09:30:29 -0300

    feat: add useForm hook to control the main form

--- app/src/pages/Login.tsx
+++ app/src/pages/Login.tsx
@@ -1,3 +1,4 @@
+import { useForm } from "react-hook-form";
 import { Button } from "../components/Button";
 import { Container } from "../components/Container";
 import { Form } from "../components/Form";
@@ -5,12 +6,18 @@ import { Heading } from "../components/Heading";
 import { Input } from "../components/Input";
 
 const Login = () => {
+
+    const { register, watch, formState: { errors, isValid } } = useForm();
+    const form = watch();
+
+    console.log(form)
+
   return (
     <Container>
       <Form>
         <Heading>Login</Heading>
-        <Input placeholder="E-mail" />
-        <Input placeholder="Senha" />
+        <Input placeholder="E-mail" {...register("email")}/>
+        <Input placeholder="Senha" {...register("password")}/>
         <Button>Entrar</Button>
       </Form>
     </Container>

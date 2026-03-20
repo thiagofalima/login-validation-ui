85616eb01526e71df22bb4ef6426f51a1df9a589
thiagofalima <thiagofalima@hotmail.com>
2026-03-20 13:02:27 -0300

    feat: pre yup schema

--- app/src/components/Input.tsx
+++ app/src/components/Input.tsx
@@ -1,22 +1,14 @@
 import { Control } from "react-hook-form";
 import styled from "styled-components";
-import { yupResolver } from "@hookform/resolvers/yup"
-import * as yup from "yup"
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
 import IInput from "../types/IInput";
 
-const schema = yup
-  .object({
-    firstName: yup.string().required(),
-    age: yup.number().positive().integer().required(),
-  })
-  .required()
-
-
 export const Input = styled.input<IInput>`
-    width: 80%;
-    padding: 10px 16px;
-    background-color: #FFFFFF;
-    color: #000000;
-    border-radius: 2em;
-    border: 2px solid #952294;
-`
+  width: 80%;
+  padding: 10px 16px;
+  background-color: #ffffff;
+  color: #000000;
+  border-radius: 2em;
+  border: 2px solid #952294;
+`;

0181e33edb9d5d07b3a3785cffd520b8c8526ad4
thiagofalima <thiagofalima@hotmail.com>
2026-03-20 14:16:45 -0300

    feat: add name property on IInput interface

--- app/src/types/IInput.tsx
+++ app/src/types/IInput.tsx
@@ -2,6 +2,7 @@ import { Control } from "react-hook-form";
 
 interface IInput extends React.InputHTMLAttributes<HTMLInputElement>{
     control: Control<any>;
+    name: string
 }
 
 export default IInput;

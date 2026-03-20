d704235745bfcb06e385c11fbe1ab858148394f6
thiagofalima <thiagofalima@hotmail.com>
2026-03-20 13:02:51 -0300

    feat: add yup form validation schema

--- app/src/components/Input.tsx
+++ app/src/components/Input.tsx
@@ -4,6 +4,19 @@ import { yupResolver } from "@hookform/resolvers/yup";
 import * as yup from "yup";
 import IInput from "../types/IInput";
 
+const schema = yup
+  .object({
+    firstName: yup
+      .string()
+      .email("E-mail inválido")
+      .required("Campo obrigarótio"),
+    age: yup
+      .string()
+      .min(6, "No mínimo 6 caracteres")
+      .required("Campo obrigatório"),
+  })
+  .required();
+
 export const Input = styled.input<IInput>`
   width: 80%;
   padding: 10px 16px;
